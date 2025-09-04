2dfa4d9da0fe660a1811ba0cc9376cbdfedf6a9a
alok-daily-dev <alokdailydev@gmail.com>
2025-09-04 12:23:48 +0000

    prompt and basic chaining has been implemented

---
+++ test.py
@@ -0,0 +1,10 @@
+# using chaining
+
+if st.button('Summarize'):
+    chain = template | model
+    result = chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
+    st.write(result.content)
